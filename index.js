9d223f354a104550c2b7b5c970ed58f93356ef00
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-01-20 13:44:13 +0530

    orgin chged

--- index.js
+++ index.js
@@ -11,13 +11,7 @@ PORT = 3000;
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(cookieParser());
 
 mongoose.connect(process.env.DB_URI);
